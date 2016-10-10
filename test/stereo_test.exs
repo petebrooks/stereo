416bfede72d78dbc43fb253ad591b53908158132
Pete Brooks <pete.brooks@tablexi.com>
2016-10-09 23:24:10 -0500

    Use describe and refute in tests

--- test/stereo_test.exs
+++ test/stereo_test.exs
@@ -14,35 +14,37 @@ defmodule StereoTest do
     end
   end
 
-  test "dry run with an array of paths" do
-    paths = Path.expand(@fixtures)
-    assert {:ok, _} = Stereo.Runner.run(:dry_run, paths)
-    assert File.exists?(@output_file) == false
-  end
+  describe "Stereo.Runner.run/2" do
+    test "dry run with an array of paths" do
+      paths = Path.expand(@fixtures)
+      assert {:ok, _} = Stereo.Runner.run(:dry_run, paths)
+      refute File.exists?(@output_file)
+    end
 
-  test "dry run with a glob string" do
-    assert {:ok, _} = Stereo.Runner.run(:dry_run, @fixtures)
-    assert File.exists?(@output_file) == false
-  end
+    test "dry run with a glob string" do
+      assert {:ok, _} = Stereo.Runner.run(:dry_run, @fixtures)
+      refute File.exists?(@output_file)
+    end
 
-  test "dry run with no files" do
-    assert {:error, "No files"} = Stereo.Runner.run(:dry_run, [])
-    assert File.exists?(@output_file) == false
-  end
+    test "dry run with no files" do
+      assert {:error, "No files"} = Stereo.Runner.run(:dry_run, [])
+      refute File.exists?(@output_file)
+    end
 
-  test "run with an array of paths" do
-    paths = Path.expand(@fixtures)
-    assert {:ok, _} = Stereo.Runner.run(:run, paths)
-    assert File.exists?(@output_file)
-  end
+    test "run with an array of paths" do
+      paths = Path.expand(@fixtures)
+      assert {:ok, _} = Stereo.Runner.run(:run, paths)
+      assert File.exists?(@output_file)
+    end
 
-  test "run with a glob string" do
-    assert {:ok, _} = Stereo.Runner.run(:run, @fixtures)
-    assert File.exists?(@output_file)
-  end
+    test "run with a glob string" do
+      assert {:ok, _} = Stereo.Runner.run(:run, @fixtures)
+      assert File.exists?(@output_file)
+    end
 
-  test "run with no files" do
-    assert {:error, "No files"} = Stereo.Runner.run(:run, [])
-    assert File.exists?(@output_file) == false
+    test "run with no files" do
+      assert {:error, "No files"} = Stereo.Runner.run(:run, [])
+      refute File.exists?(@output_file)
+    end
   end
 end
